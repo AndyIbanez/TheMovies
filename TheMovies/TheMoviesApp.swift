8defb6754d55b90ec51db7b33ef805bf58d4fd08
Andy Ibanez <codecs.elision09@icloud.com>
2024-11-28 19:51:17 -0400

    Initial Commit

---
+++ TheMovies/TheMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoviesApp.swift
+//  TheMovies
+//
+//  Created by Andy Ibanez on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
