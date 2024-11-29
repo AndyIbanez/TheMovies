e94b5dee74cbe27e80b2b3ad569e556cb7b24529
Andy Ibanez <codecs.elision09@icloud.com>
2024-11-28 20:32:51 -0400

    New extension to convert Strings as URLs

---
+++ TheMovies/Extensions/Foundation/String+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Untitled.swift
+//  TheMovies
+//
+//  Created by Andy Ibanez on 11/28/24.
+//
+
+import Foundation
+
+extension String {
+    var urlRepresentation: URL? {
+        guard let url = URL(string: self) else { return nil }
+        return url
+    }
+}
