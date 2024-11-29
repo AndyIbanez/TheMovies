3f323ee5a9f46cc48645018313eff5d0704db328
Andy Ibanez <codecs.elision09@icloud.com>
2024-11-29 15:09:25 -0400

    Process hooking logic to search screen.

--- TheMovies/UI/SearchScreen/SearchScreen.swift
+++ TheMovies/UI/SearchScreen/SearchScreen.swift
@@ -6,15 +6,47 @@
 //
 
 import SwiftUI
+import Combine
 
 @Observable
 class SearchScreenViewModel {
     var apiError: OMDBAPIError?
     var loading: Bool = false
+    var searchResults: [OMDBSearchResult] = []
+    var availableResults: Int = 0
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    func search(for query: String, page: Int, withProvider provider: MoviesProvider) {
+        loading = true
+        apiError = nil
+        
+        provider
+            .dataSource
+            .search(for: query, page: page, type: .movie)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    self.apiError = error
+                }
+            }, receiveValue: { results in
+                self.searchResults += results.results
+                self.availableResults = results.totalResults
+            }
+        )
+        .store(in: &cancellables)
+    }
 }
 
 struct SearchScreen: View {
+    @Environment(\.moviesProvider) private var moviesProvider
+    
     @State private var navigationStack: [MovieNavigationStack] = []
+    @State private var searchText: String = ""
+    @State private var currentPage: Int = 1
     
     var body: some View {
         NavigationStack(path: $navigationStack) {
@@ -22,6 +54,7 @@ struct SearchScreen: View {
                 Text("Sup")
             }
             .navigationTitle("Search")
+            .searchable(text: $searchText, prompt: "Movie title")
         }
     }
 }
