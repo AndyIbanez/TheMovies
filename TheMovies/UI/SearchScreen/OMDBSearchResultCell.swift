24783488da3f786247e5c83f9a1766a1b8fac51c
Andy Ibanez <codecs.elision09@icloud.com>
2024-11-29 18:24:27 -0400

    Search result cell

---
+++ TheMovies/UI/SearchScreen/OMDBSearchResultCell.swift
@@ -0,0 +1,59 @@
+//
+//  OMDBSearchResultCell.swift
+//  TheMovies
+//
+//  Created by Andy Ibanez on 11/29/24.
+//
+
+import SwiftUI
+
+struct OMDBSearchResultCell: View {
+    let result: OMDBSearchResult
+
+    var body: some View {
+        HStack(alignment: .top, spacing: 12) {
+
+            AsyncImage(url: URL(string: result.posterURL ?? "")) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                        .frame(width: 80, height: 120)
+                case .success(let image):
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 80, height: 120)
+                        .clipped()
+                        .cornerRadius(8)
+                case .failure:
+                    Image(systemName: "photo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 120)
+                        .foregroundColor(.gray)
+                        .cornerRadius(8)
+                @unknown default:
+                    EmptyView()
+                }
+            }
+
+
+            VStack(alignment: .leading, spacing: 4) {
+                Text(result.title)
+                    .font(.headline)
+                    .foregroundColor(.primary)
+                    .lineLimit(2)
+                    .multilineTextAlignment(.leading)
+
+                if let year = result.year {
+                    Text("Year: \(year)")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+            }
+
+            Spacer()
+        }
+        .padding(.vertical, 8)
+    }
+}
--- TheMovies/UI/SearchScreen/SearchScreen.swift
+++ TheMovies/UI/SearchScreen/SearchScreen.swift
@@ -8,54 +8,6 @@
 import SwiftUI
 import Combine
 
-@Observable
-class SearchScreenViewModel {
-    var apiError: OMDBAPIError?
-    var loading: Bool = false
-    var searchResults: [OMDBSearchResult] = []
-    var availableResults: Int = 0
-    var searchQuery: String = ""
-    var currentPage: Int = 1
-    
-    private let debounceTime: RunLoop.SchedulerTimeType.Stride = .milliseconds(300)
-    
-    private var searchCancellable: AnyCancellable?
-    private var debounceTimer: Timer?
-    
-    private func performSearch(withProvider provider: MoviesProvider) {
-        searchCancellable?.cancel()
-        loading = true
-        apiError = nil
-        
-        searchCancellable = provider
-            .dataSource
-            .search(for: searchQuery, page: currentPage, type: .movie)
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { completion in
-                self.loading = false
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    self.apiError = error
-                }
-            }, receiveValue: { results in
-                self.searchResults += results.results
-                self.availableResults = results.totalResults
-            }
-        )
-    }
-    
-    func search(withProvider provider: MoviesProvider) {
-        debounceTimer?.invalidate()
-
-        debounceTimer = Timer.scheduledTimer(withTimeInterval: debounceTime.timeInterval, repeats: false) { [weak self] _ in
-            guard let self = self else { return }
-            self.performSearch(withProvider: provider)
-        }
-    }
-}
-
 struct SearchScreen: View {
     @Environment(\.moviesProvider) private var moviesProvider
     
@@ -77,7 +29,7 @@ struct SearchScreen: View {
                     ContentUnavailableView.search(text: viewModel.searchQuery)
                 } else {
                     List(viewModel.searchResults) { result in
-                        Text(result.title)
+                        OMDBSearchResultCell(result: result)
                     }
                 }
             }
