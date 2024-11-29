607b20a2806a483b953ccdf111a3f66456b63682
Andy Ibanez <codecs.elision09@icloud.com>
2024-11-28 21:25:00 -0400

    More objects for API usage.

--- TheMovies/OMDBAPI/Providers/API Objects/OMDBAPIMovieResult.swift
+++ TheMovies/OMDBAPI/Providers/API Objects/OMDBAPIMovieResult.swift
@@ -1,35 +1,41 @@
 //
-//  OMDBSearchResult.swift
+//  OMDBAPIMovieResult.swift
 //  TheMovies
 //
 //  Created by Andy Ibanez on 11/28/24.
 //
 
-struct OMDBSearchResult: Decodable {
+import Foundation
+
+struct OMDBAPIMovieResult: Decodable {
     enum CodingKeys: String, CodingKey {
-        case id = "imdbID"
         case title = "Title"
         case year = "Year"
+        case imdbID
         case posterURL = "Poster"
+        case ratings = "Ratings"
     }
     
-    var id: String
-    var title: String
-    var year: Int?
-    var posterURL: String?
+    let title: String
+    let year: Int?
+    let imdbID: String
+    let posterURL: String?
+    let ratings: [OMDBRating]
     
-    init(id: String, title: String, year: Int, posterURL: String? = nil) {
-        self.id = id
+    init(title: String, year: Int, imdbID: String, posterURL: String?, ratings: [OMDBRating]) {
         self.title = title
         self.year = year
+        self.imdbID = imdbID
         self.posterURL = posterURL
+        self.ratings = ratings
     }
     
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try container.decode(String.self, forKey: .id)
         self.title = try container.decode(String.self, forKey: .title)
         self.year = Int(try container.decode(String.self, forKey: .year))
+        self.imdbID = try container.decode(String.self, forKey: .imdbID)
         self.posterURL = try container.decodeIfPresent(String.self, forKey: .posterURL)
+        self.ratings = try container.decode([OMDBRating].self, forKey: .ratings)
     }
 }
